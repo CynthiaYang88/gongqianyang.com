f8846e1322d581ced8f50f7196da310a66eaac62
CynthiaYang88 <cyang67@jhu.edu>
2021-06-07 04:47:09 -0500

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -4,20 +4,312 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+<div id="header" className="white">
+	<div className="wrapper">
+		<a id="logo"
+			href="index.html">Gong-Qian Yang</a>
+		<span className="mobile_menu"></span>
+		<ul className="menu">
+
+			<li className="heart">
+				<a data-anchor="highlights"   
+					href=" ">Highlights<span></span></a>
+			</li> 
+			<li><a data-anchor="album" href=" ">Album
+					Download<span></span></a></li> 
+			<li><a data-anchor="about" href=" ">Gong-Qian<span></span></a></li>
+			<li>
+				<a data-anchor="footer" href=" ">Contact<span></span></a>
+				<span id="contact-submenu" className="submenu_wrapper">
+					<ul className="submenu">
+						<li><a href="mailto:yanggongqian@yahoo.com">yanggongqian@yahoo.com</a></li>
+					</ul>
+				</span>
+			</li>
+		</ul>
+	</div>
+</div>
+
+<div id="welcome" className="section full static" style={{height:'100vh'}}>
+
+	<div className="wrapper">
+
+		<div id="company-name">Gong-Qian Yang</div>
+		<div id="key-phrases">
+			<div>Violinist</div>
+			<div>Composer</div>
+			<div>Producer</div>
+		</div>
+	</div>
+	
+	<div id="bgvid">
+	<iframe src="https://fast.wistia.net/embed/iframe/9x25lsgdqc?background=1&autoplay=1&loop=1&byline=0&title=0" frameBorder="0" msallowfullscreen webkitallowfullscreen mozallowfullscreen allowfullscreen ></iframe></div>
+
+	<svg id="scroll-me" className="mouse" data-anchor="properties" xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 76 130" preserveAspectRatio="xMidYMid meet">
+		<g fill="none" fillRule="evenodd">
+			<rect width="70" height="118" x="1.5" y="1.5" stroke="#FFF" stroke-width="3" rx="36" />
+			<circle className="scroll" cx="36.5" cy="31.5" r="7" fill="#FFF" />
+		</g>
+	</svg>
+
+
+</div>
+
+
+<div id="about" className="section minfull black">
+	<div className="wrapper">
+		<div className="content split aligntop">
+			<div className="left" style={{width:'30%', textAlign:'center'}}>
+				<div className="thumb" style={{paddingTop:'133.33%'}}><span
+						style={{backgroundImage:"url('images/20190811_081951_med-3.png')", backgroundPosition: 'left top'}}></span>
+				</div>
+			</div>
+			<div className="right" style={{width: '70%'}}>
+				<h3>Gong-Qian Yang</h3>
+				<p>Yang’s music is in 21st century modern classical music style ranging from Choral, movie sound
+					track, TV, radio headlines and gaming music, soundscape, to symphonic works such as "Nature and
+					Me " Symphonic Poem and "Dawn-Awakening", etc… His music is natural, easy listening, uplifting
+					and inspirational.</p>
+				<p>As a composer, producer and publisher, Gong Qian Yang has released seven of his music albums:
+					Sonic Poem, Sonic Spring, Sonic Wave, Lean on Me, Music for Yoga and More, Sonic Fusion, Space
+					and Stars, etc... "Space and Stars” is Yang's inspiration for communication with nature: Birds,
+					Forest, Seashore, Wave, Wind, Night sky, Moon and twinkling stars....</p>
+				<p>As an accomplished violinist, Yang has been twice first-prize winner of the Starling
+					International Scholarship Foundation in U.S. He pursued his violin study with the legendary
+					violin teacher Prof. Dorothy Delay and Prof. Kurt Sassmannshaus at both Aspen music Festival and
+					Cincinnati College Conservatory of Music (CCM). Yang has been conferred Master's Degree of
+					Music. Yang also studied with Ms. Kathleen Lenski, and Prof.Robert Lipsett earlier when he was
+					in California.</p>
+				<p>Yang very much enjoyed taking composer Joel Hoffman's class at CCM. He was also very grateful to
+					his music mentor Gerhard Samuel (who studied composition with Paul Hindemith, and studied
+					conducting with Koussevitzky). Yang was coached and influenced by Prof. Samuel on modern music
+					languages and new music for the future. Fortunately, Yang was offered the opportunity to play
+					Prof. Samuel's Gagliano violin during the time while he was studying at CCM.</p>
+				<p>While enjoying a successful career as soloist and concert artist, Yang always kept keen interest
+					in music composition. Focusing on his music production, Yang has studied a wide range of
+					repertoire and a great diversity of music by western masters. As a sensible composer, Yang has
+					often been inspired by the harmony between all the creatures in the universe. A lot of his music
+					idea came to him when he was on his music tour. He was humming along on the way to Nashville,
+					and his heart was singing when he was driving in the Appalachian Mountains. Yang says he enjoys
+					being close to the nature whether it is sunny or rainy; he feels the energy and harmony coming
+					from everywhere.</p>
+				<p>Yang expressed that music should radiate infinite life energy that glorifies the beauty of the
+					great universe and of course the beauty of humans living within.</p>
+				<p>Gong Qian Yang's music albums are available at: Apple iTunes, Spotify, Apple Music, Verizon,
+					Amazon MP3, Tencent Music, Netease Music, Nokia, My Space Music, Tradebit, Napster, Emusic, etc.
+					Yang’s music has been well recerived both in U.S. and internationally, for his music is reaching
+					and touching human hearts in a very intimate way...</p>
+			</div>
+		</div>
+	</div>
+	<div className="mask black deep"></div>
+</div>
+
+<div id="highlights" className="section minfull black">
+	<div className="wrapper">
+		<div className="content">
+			<div className="content split aligntop">
+				<div className="left" style={{width:'30%',textAlign: 'center'}} >
+					<div className="thumb" style={{paddingTop: '133.33%'}}><span
+							style={{backgroundImage: 'url()', backgroundPosition: 'left top'}}></span>
+					</div>
+				</div>
+				<div className="right" style={{width:'70%'}}>
+					<h3 className="heavy">Highlights</h3>
+					<p>Musical Highlights</p>
+					<p id="mcetoc_1cq6r8t431"> On August 8, 2019 Bravo Chicago International Summer Music Festival’s
+						Gala Concert event, Gong Qian Yang’s original symphonic work “Dawn-Awakening” was presented
+						at Chicago William E. And Mary Pat Gannon Hay Concert Hall of DePaul University. This
+						symphonic work, for the very first time, with choir, and audience participation and
+						seamlessly working together, has created a magnificent music which inspired and touched the
+						audience in a very intimate way... Everyone in the concert hall became part of this exciting
+						music! Thanks for the great support from the festival committee, world renowned tenors Allan
+						Glassman, Tao Tong and great soprano Vanessa Liao to make this event a huge success and
+						memorable one for all!</p>
+					<p>On Feb 10, 2018, American Music Festivals and the Chicago Chopin Society brought the 2017-18
+						International Cultural Exchange Festival to a high note finale with Chinese New Year Concert
+						at St. Barbara Catholic Parish, Chicago. In this concert, Gong Qian Yang’s original music
+						composition "Fantasy for Violin and Piano with String Orchestra” was world premired by
+						American Music Festivals and Lincolnwood Chamber Orchestra, with Claire Arias-Kim, Violin,
+						and Jaroslaw Golembiowski, Piano. The concert was on NBC news and Chicago WGN the same
+						evening. This event is co-sponsored by Chicago Chinatown Chamber of Commerce, Polish and
+						Slavic Federal Credit Union, and Loeber Motors.</p>
+					<p>On Saturday Jan 21, 2017, Gong Qian Yang’s original music compositions were presented by
+						American Music Festivals, Lincolnwood Chamber Orchestra and Chicago Public Library at 2017
+						Chicago Chinese New Year Concert, co-sponsored by Chicago Chinatown Chamber of commerce. The
+						conductor, composer, orchestra, and the wonderful huge crowd of audience, all have been
+						inspired by the music and by the positive, uplifting sentiment created together - The
+						concert, with the music composed right here in Chicago has received warm standing ovation!</p>
+					<p>Gong Qian Yang’s 7th album “Sonic Fusion” was released in December 2016! In this album,
+						Composer's effort to discover and unlock the emotional meaning of sound waves, pitches
+						including 12 tone rows from a fresh new perspective. Clearly defined pitches and non-defined
+						pitches were both explored in the music process with listener friendly in mind.</p>
+			  
+				</div>
+			</div>
+		</div>
+	</div>
+	<div className="mask black"></div>
+</div>
+
+
+<div id="album" className="section minfull white">
+	<div className="wrapper center">
+		
+		<div className="content">
+			<h3>Album Download</h3>
+			<div className="card_container community_cards">
+
+				<div id='area-card-93' className='card area_card public' data-tags=''>
+					<a href='https://music.apple.com/us/album/sonic-fusion/1186015491' 
+						className='wrapper card_content'	target='_self'>
+						<span className='thumb area_thumb'>
+
+							<span className='image_container'><span role='img' aria-label=''
+								data-lazy='images\sonic-fusionby-gong-qian_med-3.jpeg'></span></span>
+						</span>
+						<span className='area_info'>
+							<span className='area_name'>Sonic Fusion</span>
+						</span>
+					</a>
+				</div>
+
+				<div id='area-card-99' className='card area_card public' data-tags=''>
+					<a href='https://music.apple.com/us/album/space-and-stars/1042687393'
+						className='wrapper card_content'  target='_self'>
+						<span className='thumb area_thumb'>
+
+							<span className='image_container'><span role='img' aria-label='' 
+								data-lazy='images\space-and-stars_med-2.jpeg'></span></span>
+						</span>
+						<span className='area_info'>
+							<span className='area_name'>Space and Stars</span>
+						</span>
+					</a>
+				</div>
+
+				<div id='area-card-90' className='card area_card public' data-tags=''>
+					<a href='https://music.apple.com/us/album/music-for-yoga-and-more/911655994' className='wrapper card_content'
+						target='_self'>
+						<span className='thumb area_thumb'>
+
+							<span className='image_container'><span role='img' aria-label=''
+									data-lazy='images\music-for-yoga-and-more_med-2.jpeg'></span></span>
+						</span>
+						<span className='area_info'>
+							<span className='area_name'>Music For Yoga and More</span>
+						</span>
+					</a>
+				</div>
+
+				<div id='area-card-15' className='card area_card public' data-tags=''>
+					<a href='https://music.apple.com/us/album/lean-on-me/910762981' className='wrapper card_content'
+						target='_self'>
+						<span className='thumb area_thumb'>
+
+							<span className='image_container'><span role='img' aria-label=''
+									data-lazy='images\final-cover_med-2.jpeg'></span></span>
+						</span>
+						<span className='area_info'>
+							<span className='area_name'>Lean On Me</span>
+						</span>
+					</a>
+				</div>
+
+				<div id='area-card-27' className='card area_card public' data-tags=''>
+					<a href='https://music.apple.com/us/album/sonic-wave-by-gong-qian-yang/715598762' className='wrapper card_content'
+						target='_self'>
+						<span className='thumb area_thumb'>
+
+							<span className='image_container'><span role='img' aria-label=''
+									data-lazy='images\sonic-wave-by-gongqian-yang_med-2.jpeg'></span></span>
+						</span>
+						<span className='area_info'>
+							<span className='area_name'>Sonic Wave</span>
+						</span>
+					</a>
+				</div>
+
+				<div id='area-card-81' className='card area_card public' data-tags=''>
+					<a href='https://music.apple.com/us/album/sonic-spring-by-gong-qian-yang/498649946' className='wrapper card_content'
+						target='_self'>
+						<span className='thumb area_thumb'>
+
+							<span className='image_container'><span role='img' aria-label=''
+									data-lazy='images\sonic-spring-album-front_med-3.jpeg'></span></span>
+						</span>
+						<span className='area_info'>
+							<span className='area_name'>Sonic Spring</span>
+						</span>
+					</a>
+				</div>
+
+				<div id='area-card-114' className='card area_card public' data-tags=''>
+					<a href='https://music.apple.com/us/album/sonic-poem/128800212'
+						className='wrapper card_content' target='_self'>
+						<span className='thumb area_thumb'>
+
+							<span className='image_container'><span role='img' aria-label=''
+									data-lazy='images\sonic-poem-by-gong-qian_med-2.jpeg'></span></span>
+						</span>
+						<span className='area_info'>
+							<span className='area_name'>Sonic Poem</span>
+						</span>
+					</a>
+				</div>
+   
+			</div>
+			<a href="https://music.apple.com/us/artist/gong-qian-yang/498649947" className="button transparent go"><span></span>Browse all compositions</a>
+		</div> 
+	</div>
+	<div className="mask white muted"></div>
+</div>
+
+
+<div id="footer" className="section black static">
+	<div className="wrapper">
+		<div id="brokerage-info">
+			<div className="broker">Gong-Qian Yang</div>
+			<div className="contact_info">
+				<div className="email"><a href="mailto:yanggongqian@yahoo.com">yanggongqian@yahoo.com</a></div>
+			</div>
+			<div className="contact_info">
+				<div className="mlsid">All Rights Reserved © Gong Qian Yang 2003 - 2021</div>
+			</div>
+		</div>
+
+		<ul className='social_icons'>
+			
+			<li className='linkedin'><a href='https://www.linkedin.com/in/yanggongqian/' target='_blank'
+					title='Linkedin' rel='noopener'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+						viewBox="0 0 24 24">
+						<path
+							d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
+					</svg></a></li>
+		   
+			<li className='youtube'><a href='https://www.youtube.com/results?search_query=gong-qian+yang' target='_blank' title='Youtube'
+					rel='noopener'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+						viewBox="0 0 24 24">
+						<path
+							d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
+					</svg></a></li>
+		</ul>
+	</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
